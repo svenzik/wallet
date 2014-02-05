ac8b7904a96ac4e613eda2888e97287ba151084b
Sven Mitt <svenzik@gmail.com>
2014-02-05 11:33:08 +0200

    Common class for IntegrationTests at Service level

---
+++ src/test/java/com/playtech/wallet/service/ServiceIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.playtech.wallet.service;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/wallet-servlet.xml")
+public class ServiceIntegrationTest {
+
+    private MockMvc mockMvc;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    protected WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.wac).build();
+    }
+
+
+    public MockMvc getMockMvc() {
+        return mockMvc;
+    }
+}
