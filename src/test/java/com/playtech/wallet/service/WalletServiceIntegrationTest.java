3365f7d9d673f30cde0ae05f461279bc422059f5
Sven Mitt <svenzik@gmail.com>
2014-02-07 12:19:31 +0200

    Updated test

--- src/test/java/com/playtech/wallet/service/WalletServiceIntegrationTest.java
+++ src/test/java/com/playtech/wallet/service/WalletServiceIntegrationTest.java
@@ -4,6 +4,7 @@ import com.playtech.wallet.domain.Player;
 import com.playtech.wallet.domain.PlayerFactory;
 import com.playtech.wallet.repository.PlayerRepository;
 import com.playtech.wallet.domain.messages.WalletChangeMessage;
+import com.playtech.wallet.repository.exceptions.PlayerNotFoundException;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,6 +16,7 @@ import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -28,21 +30,33 @@ public class WalletServiceIntegrationTest extends ServiceIntegrationTest {
     @Autowired
     private PlayerRepository playerRepository;
 
-    private final String PLAYER_USERNAME = "username";
+    private final String PLAYER_USERNAME = "WalletServiceIntegrationTest";
 
-    private List<Exception> exceptionList = new ArrayList<Exception>();
+    private final static List<Exception> exceptionList = new ArrayList<Exception>();
+
+    private final AtomicLong sum = new AtomicLong();
+    private final AtomicLong count = new AtomicLong();
+
 
     private Player getAndResetPlayerFromRepository() {
-        Player player = playerRepository.findByUsername(PLAYER_USERNAME);
-        if (player == null) {
+
+        Player player;
+        try {
+            player = playerRepository.findByUsername(PLAYER_USERNAME);
+        } catch (PlayerNotFoundException e) {
             player = PlayerFactory.createNewPlayer(PLAYER_USERNAME);
             playerRepository.save(player);
         }
+
         //make the balance 0.53
         player.changeBalance(BigDecimal.ZERO.subtract(player.getBalance()).add(BigDecimal.valueOf(53,2)));
         return playerRepository.save(player);
     }
 
+    /**
+     * Modifies the player
+     * @throws Exception if test fails
+     */
     @Test
     public void testChangeBalance() throws Exception {
 
@@ -62,59 +76,64 @@ public class WalletServiceIntegrationTest extends ServiceIntegrationTest {
             throw new Exception("Test failed to end in time", e);
         }
 
-        //every thread adds 11 to balance
-        assertEquals(playerState.getBalance().add(BigDecimal.valueOf(threads * 11)),
-                    playerRepository.findByUsername(PLAYER_USERNAME).getBalance());
 
+        Player player = playerRepository.findByUsername(PLAYER_USERNAME);
+        String format = "sum(%s)-count(%s)";
+
+        String sumAndCountRepository = String.format(format, player.getBalance(), player.getVersion()-playerState.getVersion());
+        String sumAndCountExpected = String.format(format,
+                                                   playerState.getBalance().add(BigDecimal.valueOf(sum.get())),
+                                                   count.get());
+
+        assertEquals(sumAndCountExpected, sumAndCountRepository);
+
+        assertEquals(0, exceptionList.size());
         assertThat(exceptionList, is((List<Exception>)new ArrayList<Exception>()));
 
     }
 
-
-
     private class BalanceUpdater implements Runnable {
-
 
         @Override
         public void run() {
-//            BigDecimal sum = getAndResetPlayerFromRepository().getBalance();
-            Long lastVersion = getAndResetPlayerFromRepository().getVersion();
 
-            for (int i = 11; i>=-10; i--) {
+            for (int i = 10; i>=-10; i--) {
+
+                BigDecimal balanceChange = BigDecimal.valueOf(i);
+
+                if (i == 0) {
+                    //with 0 there is no transaction, so lets change that
+                    balanceChange = BigDecimal.valueOf(13);
+                }
+
+                sum.addAndGet(balanceChange.longValue());
+                count.getAndIncrement();
 
                 UUID transactionId = UUID.randomUUID();
-                Long probableNewVersion = lastVersion + 1L;
 
                 WalletChangeMessage message = new WalletChangeMessage();
                 message.setUsername(PLAYER_USERNAME);
                 message.setTransactionId(transactionId);
-                message.setBalanceChange(BigDecimal.valueOf(i));
-
+                message.setBalanceChange(balanceChange);
 
                 try {
 
-                  getMockMvc().perform(post("/wallet")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(convertObjectToJsonBytes(message)))
+                  getMockMvc().perform(
+                          post("/wallet")
+                          .contentType(MediaType.APPLICATION_JSON)
+                          .content(convertObjectToJsonBytes(message)))
 //                            .andDo(print())
                             .andExpect(status().isOk())
                             .andExpect(jsonPath("$.transactionId").value(transactionId.toString()))
-                                    .andExpect(jsonPath("$.errorCode").value(nullValue()))
-//                            .andExpect(content().string("errorCode"))
-//                            .andExpect(jsonPath("$.balanceVersion", is(probableNewVersion)))
-                            .andExpect(jsonPath("$.balanceChange").value(BigDecimal.valueOf(i).intValue())) //.intValue() otherwise fails
+                            .andExpect(jsonPath("$.errorCode").value("OK"))
+                            .andExpect(jsonPath("$.balanceChange").value(balanceChange.intValue())) //.intValue() otherwise fails
                             .andReturn();
 
 
                 } catch (Exception e) {
                     e.printStackTrace();
                     exceptionList.add(e);
-//                    throw new RuntimeException(e);
                 }
-
-
-
-
             }
         }
 
--- src/test/java/com/playtech/wallet/service/WalletServiceTest.java
+++ src/test/java/com/playtech/wallet/service/WalletServiceTest.java
@@ -2,32 +2,49 @@ package com.playtech.wallet.service;
 
 import com.playtech.wallet.domain.Player;
 import com.playtech.wallet.domain.PlayerFactory;
+import com.playtech.wallet.domain.messages.WalletChangeResultStatus;
+import com.playtech.wallet.domain.transactions.PlayerTransaction;
 import com.playtech.wallet.repository.PlayerRepository;
 import com.playtech.wallet.domain.messages.WalletChangeMessage;
 import com.playtech.wallet.domain.messages.WalletChangeResult;
+import com.playtech.wallet.repository.PlayerTransactionRepository;
+import com.playtech.wallet.repository.exceptions.PlayerNotFoundException;
 import junit.framework.Assert;
-import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.dao.OptimisticLockingFailureException;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class WalletServiceTest extends TestCase {
+public class WalletServiceTest {
 
-
-    private PlayerRepository repository = new PlayerRepository() {
+    private PlayerRepository playerRepository = new PlayerRepository() {
 
         private Player player = PlayerFactory.createNewPlayer("username");
+        private final String lockUsername = WalletChangeResultStatus.OPTIMISTIC_LOCKING_EXCEPTION.toString();
 
         @Override
-        public Player findByUsername(String username) {
-            return player;
+        public Player findByUsername(String username) throws PlayerNotFoundException{
+            if (player.getUsername().equals(username)) {
+                return player;
+            }
+            if (username.equals(lockUsername)) {
+                return PlayerFactory.createNewPlayer(lockUsername);
+            }
+            throw new PlayerNotFoundException(username);
         }
 
         @Override
         public Player save(Player entity) {
+            if (lockUsername.equals(entity.getUsername())) {
+                throw new OptimisticLockingFailureException("TEST EXCEPTION");
+            }
+            Player tmpPlayer = PlayerFactory.createNewPlayer(entity.getUsername());
+            tmpPlayer.changeBalance(entity.getBalance());
+            player = tmpPlayer;
             return player;
         }
 
@@ -41,36 +58,148 @@ public class WalletServiceTest extends TestCase {
 
     };
 
-    private WalletService walletService = new WalletService(repository);
+    private PlayerTransactionRepository transactionRepository = new PlayerTransactionRepository() {
 
-    private Player getUser() {
-        return repository.findByUsername("username");
+        private List<UUID> transactionTable = new ArrayList<UUID>();
+
+        @Override
+        public PlayerTransaction findByTransactionId(UUID transaction) {
+            for (UUID uuid : transactionTable) {
+                if (uuid.equals(transaction)) {
+                    return new PlayerTransaction(uuid);
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public PlayerTransaction save(PlayerTransaction transaction) {
+            this.transactionTable.add(transaction.getTransactionId());
+            return transaction;
+        }
+    };
+
+    private WalletService walletService = new WalletService(playerRepository, transactionRepository);
+
+    private WalletChangeMessage incomingMessage = new WalletChangeMessage();
+
+    private Player getUser() throws PlayerNotFoundException {
+        return playerRepository.findByUsername("username");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+        BigDecimal balanceChange = BigDecimal.valueOf(11.34);
+        UUID transactionId = UUID.randomUUID();
+
+        incomingMessage.setUsername("username");
+        incomingMessage.setTransactionId(transactionId);
+        incomingMessage.setBalanceChange(balanceChange);
+
     }
 
     @Test
-    public void testChangeBalance() throws Exception {
+    public void testChangeBalanceOk() throws Exception {
 
         BigDecimal startBalance = getUser().getBalance();
+        BigDecimal balanceChange = incomingMessage.getBalanceChange();
 
-        UUID transactionId = UUID.randomUUID();
-        BigDecimal balanceChange = new BigDecimal(11.34);
+        WalletChangeResult result = walletService.changeBalance(incomingMessage);
 
-        WalletChangeMessage message = new WalletChangeMessage();
-        //change values
-        message.setUsername("username");
-        message.setTransactionId(transactionId);
-        message.setBalanceChange(balanceChange);
+        assertAllwaysTrueWalletService(incomingMessage, result);
+        Assert.assertEquals(WalletChangeResultStatus.OK, result.getErrorCode());
 
-        WalletChangeResult result = walletService.changeBalance(message);
+        //calculate balance change
+        BigDecimal expectedBalance = startBalance.add(balanceChange);
+        Assert.assertEquals(expectedBalance, result.getTotalBalance());
 
-        Assert.assertEquals(transactionId, result.getTransactionId());
-        Assert.assertNull(result.getErrorCode());
-        Assert.assertEquals(balanceChange, result.getBalanceChange());
+        incomingMessage.setTransactionId(UUID.randomUUID());
+        result = walletService.changeBalance(incomingMessage);
+
+        assertAllwaysTrueWalletService(incomingMessage, result);
+        Assert.assertEquals(WalletChangeResultStatus.OK, result.getErrorCode());
+
+        //calculate 2xbalance change
+        expectedBalance = expectedBalance.add(balanceChange);
+        Assert.assertEquals(expectedBalance, result.getTotalBalance());
+
+    }
+
+    @Test
+    public void testChangeBalanceRepeatingTransaction() throws Exception {
+
+        BigDecimal startBalance = getUser().getBalance();
+        BigDecimal balanceChange = incomingMessage.getBalanceChange();
+
+        WalletChangeResult result = walletService.changeBalance(incomingMessage);
+
+        assertAllwaysTrueWalletService(incomingMessage, result);
+        Assert.assertEquals(result.getErrorCode(), WalletChangeResultStatus.OK);
 
         BigDecimal expectedBalance = startBalance.add(balanceChange);
         Assert.assertEquals(expectedBalance, result.getTotalBalance());
+
+        result = walletService.changeBalance(incomingMessage);
+
+        assertAllwaysTrueWalletService(incomingMessage, result);
+        Assert.assertEquals(WalletChangeResultStatus.REPEATING_TRANSACTION, result.getErrorCode());
+
+        //total balance must not change
+        Assert.assertEquals(expectedBalance, result.getTotalBalance());
+
+    }
+
+    @Test
+    public void testChangeBalanceOptimisticLockingException() throws Exception {
+
+        BigDecimal startBalance = getUser().getBalance();
+
+        incomingMessage.setUsername(WalletChangeResultStatus.OPTIMISTIC_LOCKING_EXCEPTION.toString());
+
+        WalletChangeResult result = walletService.changeBalance(incomingMessage);
+
+        assertAllwaysTrueWalletService(incomingMessage, result);
+        Assert.assertEquals(WalletChangeResultStatus.OPTIMISTIC_LOCKING_EXCEPTION, result.getErrorCode());
+
+        //total balance must not change
+        Assert.assertEquals(startBalance, result.getTotalBalance());
+
+    }
+    @Test
+    public void testChangeBalanceNoSuchPlayer() throws Exception {
+
+        incomingMessage.setUsername("NO_SUCH_USERERNAME");
+
+        WalletChangeResult result = walletService.changeBalance(incomingMessage);
+
+        assertAllwaysTrueWalletService(incomingMessage, result);
+        Assert.assertEquals(WalletChangeResultStatus.NO_SUCH_PLAYER, result.getErrorCode());
+
+        //non existin users balance is null
+        Assert.assertNull(result.getTotalBalance());
+
     }
 
 
+    @Test
+    public void testChangeBalanceMoreThenAvailable() throws Exception {
 
+        BigDecimal startBalance = getUser().getBalance();
+
+        incomingMessage.setBalanceChange(startBalance.subtract(BigDecimal.ONE));
+
+        WalletChangeResult result = walletService.changeBalance(incomingMessage);
+
+        assertAllwaysTrueWalletService(incomingMessage, result);
+        Assert.assertEquals(WalletChangeResultStatus.PLAYER_BALANCE_LESS_THAN_ZERO, result.getErrorCode());
+
+        Assert.assertEquals(startBalance, result.getTotalBalance());
+
+    }
+
+    private void assertAllwaysTrueWalletService(WalletChangeMessage incomingMessage, WalletChangeResult result) {
+        Assert.assertEquals(incomingMessage.getTransactionId(), result.getTransactionId());
+        Assert.assertEquals(incomingMessage.getBalanceChange(), result.getBalanceChange());
+    }
 }
